289ef993b8dbf187130953f66e0f5d8db432a9af
Akagigahara <leon.wichern@hotmail.com>
2023-04-03 18:50:28 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Essensplangenerator
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			AddRecipeWindow RecipeWindow = new();
+			RecipeWindow.Show();
+		}
+	}
+}
